635b06e815b4d3217c29a080f359da9229fcbfce
WangHao <wanghao.xznu@gmail.com>
2014-01-21 18:28:42 +0800

    at the second day

--- tpct.sh
+++ tpct.sh
@@ -1,4 +1,18 @@
 echo "Toolchain performance comparison test cases"
+MEMCPY_REF=940
+WHETSTONE_REF=680.683
+WHETS_NEON_REF=346.287
+
+
+MEMCPY_REF_4_6_2
+WHETSTONE_REF_4_6_2=717.893 
+WHETS_NEON_REF_4_6_2=86.178
+
+MEMCPY_REF_4_7_2
+WHETSTONE_REF_4_7_2= 
+WHETS_NEON_REF_4_7_2=
+
+
 
 
 setup()
@@ -16,7 +30,7 @@ cleanup()
     RC=0
     #TODO add cleanup code here
     #################################
-	
+	###########需要在cleanup里面把log文件给cat出来##############	
 }
 ##################################################
 usage()
@@ -25,18 +39,7 @@ usage()
     echo "1: performance comparison for memcpybm"
 	echo "2: performance comparison for whetstone"
     echo "3: performance comparison for whets_neon"	
-    echo "4: concurrent memtester and CMA"	
 }
-##################################################
-reference_table()
-{
-    echo "memcpy performance 1024KBytes  951MB/sec"
-    echo "1: performance comparison for memcpybm"
-	echo "2: performance comparison for whetstone"
-    echo "3: performance comparison for whets_neon"	
-    echo "4: concurrent memtester and CMA"	
-}
-
 ##################################################
 test_case_01()
 {
@@ -45,15 +48,24 @@ test_case_01()
     #print test info
     tst_resm TINFO "test $TST_COUNT: $TCID "
 	###################### by wh #######################
+	memcpybm > memcpy.log
+	sleep 1
+	MEMCPY_CURRENT=$(cat memcpy.log | grep "1024" | cut -c 19-21)
+	sleep 1
+	echo $MEMCPY_CURRENT
+	rm memcpy.log
+	sleep 1
+	MEMCPY_TMP=$(( $MEMCPY_CURRENT - $MEMCPY_REF ))
 	
-    #nor_mtd_testapp -T RDRW -D $mtdnode -L 0x${mtdsize} -V || return $RC
-	
-	memcpybm 1
-	
-	
+	MEMCPY_TMP=${MEMCPY_TMP#-}
+	MEMCPY_THRESHOLD=10                                ##################15 is not good can change by auto#################
+	if [ $MEMCPY_TMP -gt $MEMCPY_THRESHOLD ];then
+	return $RC
+	fi
 	###################### by wh #######################
     return 0
 }
+
 ##################################################
 test_case_02()
 {
@@ -61,12 +73,24 @@ test_case_02()
     RC=2
     #print test info
     tst_resm TINFO "test $TST_COUNT: $TCID "
-    flash_eraseall $mtdnode 
-    ret=$(hexdump $mtdnode | grep ffff | wc -l)
-    if [ $ret -eq 1 ]; then
-        RC=0
-    fi
-    return $RC
+	###################### by wh #######################	
+	whetstone < auto_input
+	sleep 1
+	WHETSTONE_CURRENT=$(cat whets.res | grep "Results" |cut -c 51-57 |sed -n '$p')
+	sleep 1
+	echo "WHETSTONE_CURRENT====================$WHETSTONE_CURRENT"
+	######rm memcpy.log######
+	sleep 1
+    WHETSTONE_TMP=$(echo "scale=3;$WHETSTONE_CURRENT - $WHETSTONE_REF"|bc|cut -c 0-6)
+    WHETSTONE_TMP=${WHETSTONE_TMP#-}
+	WHETSTONE_THRESHOLD=10                                ##################15 is not good can change by auto#################
+    echo "WHETSTONE_TMP====================$WHETSTONE_TMP"	
+    #if [$(echo "$WHETSTONE_TMP > $WHETSTONE_THRESHOLD"|bc) -eq 1 ];then
+    if [ `echo "$WHETSTONE_TMP > $WHETSTONE_THRESHOLD"|bc` -eq 1 ];then
+	return $RC
+	fi
+    ###################### by wh #######################
+    return 0
 }
 ##################################################
 test_case_03()
@@ -75,8 +99,23 @@ test_case_03()
     RC=3
     #print test info
     tst_resm TINFO "test $TST_COUNT: $TCID "
-    echo "nor performance test"
-    nor_mtd_testapp -T PERFORM -D $mtdnode -V || return $RC
+	###################### by wh #######################
+	
+	
+	whets_neon < auto_input
+	sleep 1
+	WHETS_NEON_CURRENT=$(cat whets.res | grep "Results" |cut -c 51-57 |sed -n '$p')
+	sleep 1
+	echo "WHETS_NEON_CURRENT====================$WHETS_NEON_CURRENT"
+	sleep 1
+	WHETS_NEON_TMP=$(echo "scale=3;$WHETS_NEON_CURRENT - $WHETS_NEON_REF"|bc|cut -c 0-6)
+    WHETS_NEON_TMP=${WHETSTONE_TMP#-}
+	WHETS_NEON_THRESHOLD=10
+	echo "WHETS_NEON_TMP====================$WHETS_NEON_TMP"	
+	if [ `echo "$WHETS_NEON_TMP > $WHETS_NEON_THRESHOLD"|bc` -eq 1 ];then
+	return $RC
+	fi
+	###################### by wh #######################
     return 0
 }
 # main function
@@ -100,7 +139,7 @@ case "$1" in
   test_case_02 || exit $RC 
   ;;
 3)
-  test_case_08 || exit $RC
+  test_case_03 || exit $RC
   ;;
 *)
   usage
